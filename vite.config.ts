cece73796b61c358860eb58e2cf0220fa096e4c8
Vortrex1 <danylo.kachmarskyi@oa.edu.ua>
2026-04-16 20:42:58 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  
+  // Базовий шлях:
+  // - На GitHub Pages = /lab-1-setup/ (назва твого репозиторію)
+  // - На Vercel = / (корінь)
+  base: process.env.VERCEL === '1' ? '/' : '/lab-1-setup/',
+})
